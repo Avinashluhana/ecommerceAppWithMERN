a0dc63b3fbe711805ad2eb07c2c0095a80ad681e
avinashluhana <aviluhana@gmail.com>
2022-07-22 19:00:48 +0500

    database setuped

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const app = require("./app");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
 
+dotenv.config({ path: "backend/config/config.env" });
 
-dotenv.config({path: "backend/config/config.env"});
-
-app.listen(process.env.PORT, ()=> {
-    console.log(`server is working on port ${process.env.PORT}`)
-})
+connectDatabase();
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`server is working on port`);
+});
