bfacd10b7eff103b764587fbf2840c6f6f63086f
avinashluhana <aviluhana@gmail.com>
2022-07-24 20:30:53 +0500

    added catcher of async error fun

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,37 +1,39 @@
 const Product = require("../models/productModel");
+const catchAsyncError = require("../utils/catchAsyncError");
 const ErrorHandler = require("../utils/errorHandler");
 
 //Creating product
 
-exports.createProduct = async (req, res, next) => {
+exports.createProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.create(req.body);
   res.status(201).json({
     success: true,
     product,
   });
-};
+});
 // get All products
 
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = catchAsyncError(async (req, res) => {
   const products = await Product.find();
-  if(!products){
+  if (!products) {
     return next(new ErrorHandler("Product not found", 4000));
   }
   res.status(200).json({
     success: true,
     products,
   });
-};
+});
 
 // Update products == admin
 
-exports.updateProduct = async (req, res, next) => {
+exports.updateProduct = catchAsyncError(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
   if (!product) {
     return res.status(500).json({
       success: false,
       message: "Product not found",
-    });  }
+    });
+  }
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -41,9 +43,9 @@ exports.updateProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
 
-exports.deleteProduct = async (req, res, next) => {
+exports.deleteProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
     return res.status(500).json({
@@ -57,11 +59,11 @@ exports.deleteProduct = async (req, res, next) => {
     success: true,
     message: "Product has been deleted",
   });
-};
+});
 
 // getting single product
 
-exports.getSingleProduct = async (req, res, next) => {
+exports.getSingleProduct = catchAsyncError(async (req, res, next) => {
   const product = await Product.findById(req.params.id);
   if (!product) {
     res.status(500).json({
@@ -74,4 +76,4 @@ exports.getSingleProduct = async (req, res, next) => {
     success: true,
     product,
   });
-};
+});
