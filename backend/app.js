76916128be6d13eb53b539fc10343de8949e8da7
avinashluhana <aviluhana@gmail.com>
2022-07-26 22:11:46 +0500

    used bcrypt to hash the password

--- backend/app.js
+++ backend/app.js
@@ -5,10 +5,10 @@ const app = express();
 app.use(express.json());
 
 const product = require("./routes/productRoute");
-const user = require("./routes/userRouter")
+const user = require("./routes/userRouter");
 
 app.use("/api/v1", product);
-app.use("/api/v1", user)
+app.use("/api/v1", user);
 
 // error middle ware
 
