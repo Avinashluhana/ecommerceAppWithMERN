2f5e6c9381a98b201e822af9d002f76da1849771
avinashluhana <aviluhana@gmail.com>
2022-08-02 21:05:35 +0500

    addeed footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
-import Header from "./component/layout/Header";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Header from "./component/layout/Header/Header";
+import { BrowserRouter as Router } from "react-router-dom";
 import WebFont from "webfontloader";
+import Footer from "./component/layout/Footer/Footer";
 
 const App = () => {
-
   useEffect(() => {
     WebFont.load({
       google: {
         families: ["Roboto", "Droid Sans", "Chilanka"],
       },
     });
-
   }, []);
 
   return (
     <div className="App">
       <Router>
         <Header />
+        <Footer />
       </Router>
     </div>
   );
