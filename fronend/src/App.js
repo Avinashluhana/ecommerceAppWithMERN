577f8b77463c784db5231209f384b263dc8d46ce
avinashluhana <aviluhana@gmail.com>
2022-08-02 18:18:07 +0500

    started fro frontend

---
+++ fronend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import Header from "./component/layout/Header";
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
