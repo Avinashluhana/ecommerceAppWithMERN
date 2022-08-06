7fe8ac392f8ad167ccc83453cc0f1c4f8516d8ef
avinashluhana <aviluhana@gmail.com>
2022-08-07 02:10:25 +0500

    added react alert for errors

--- frontend/src/component/Home/Home.js
+++ frontend/src/component/Home/Home.js
@@ -6,15 +6,20 @@ import MetaData from "../layout/MetaData";
 import { getProduct } from "../../Redux/actions/productAction";
 import { useDispatch, useSelector } from "react-redux";
 import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
 
 const Home = () => {
+  const alert = useAlert();
   const dispatch = useDispatch();
   const { loading, error, products, productCount } = useSelector(
     (state) => state.products
   );
   useEffect(() => {
+    if (error) {
+      return alert.error(error);
+    }
     dispatch(getProduct());
-  }, [dispatch]);
+  }, [dispatch, error]);
   return (
     <Fragment>
       {loading ? (
