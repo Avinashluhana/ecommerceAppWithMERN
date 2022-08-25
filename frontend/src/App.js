1e0813d8787405b95afa053fa54810930e6caab4
avinashluhana <aviluhana@gmail.com>
2022-08-25 22:26:58 +0500

    added search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import About from "./component/About/About";
 import ProductDetails from "./component/Product/ProductDetails";
 import Products from "./component/Product/Products";
+import Search from "./component/Product/Search";
 const App = () => {
   useEffect(() => {
     WebFont.load({
@@ -26,6 +27,7 @@ const App = () => {
           <Route path="/about" element={<About />} exact />
           <Route path="/product/:id" element={<ProductDetails />} exact />
           <Route path="/products" element={<Products />} exact />
+          <Route path="/search" element={<Search />} exact />
         </Routes>
         <Footer />
       </BrowserRouter>
