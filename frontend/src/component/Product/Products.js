6a894362832b541625d1eb91342befc91a80f21c
avinashluhana <aviluhana@gmail.com>
2022-08-25 22:12:52 +0500

    added products page

--- frontend/src/component/Product/Products.js
+++ frontend/src/component/Product/Products.js
@@ -1,17 +1,35 @@
 import React, { Fragment } from "react";
+import "./Products.css"
 import Loader from "../layout/Loader/Loader";
 import { useDispatch, useSelector } from "react-redux";
 import { clearErrors, getProduct } from "../../Redux/actions/productAction";
 import { useEffect } from "react";
+import ProductCard from "../Home/ProductCard";
 const Products = () => {
-  const disptach = useDispatch();
+  const dispatch = useDispatch();
   const { products, loading, error, productsCount } = useSelector(
     (state) => state.products
   );
-  useEffect = (() => {
-    disptach(getProduct())
-  },[disptach])
-  return <Fragment>{loading ? <Loader /> : <Fragment></Fragment>}</Fragment>;
+  useEffect(() => {
+      dispatch(getProduct());
+    },[dispatch]);
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <h2 className="productsHeading">Products</h2>
+          <div className="products">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 export default Products;
