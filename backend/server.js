59fbd59dfd8360c567162bbe83b1b5412025e738
avinashluhana <aviluhana@gmail.com>
2022-08-29 17:21:57 +0500

    added cloudniary server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
+const cloudinary = require("cloudinary")
 
 //Handling unCaught exception
 
@@ -14,6 +15,15 @@ process.on("uncaughtException", (err) => {
 dotenv.config({ path: "backend/config/config.env" });
 
 connectDatabase();
+
+// cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
 const PORT = 5000;
 const server = app.listen(PORT, () => {
   console.log(`server is working on port 5000`);
